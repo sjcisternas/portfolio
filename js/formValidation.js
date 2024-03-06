2aad4ac886d02b7264c05c1326b526a41f7b01bc
santi1408 <cisternassj@gmail.com>
2024-03-06 12:31:00 -0300

    falta acomodar la parte de mi trabajo y listo

--- js/formValidation.js
+++ js/formValidation.js
@@ -61,8 +61,31 @@ form.addEventListener('submit', (e) => {
     mostrarExito();
 });
 
+
+
 //mostrar exito
 const mostrarExito = () => {
+    const sendMail = () => {
+        let params = {
+            name: document.getElementById('form-nombre').value,
+            email: document.getElementById('form-mail').value,
+            msg: document.getElementById('form-msg').value
+        }
+    
+        const serviceID = 'service_0woeck8';
+        const templateID = 'template_xo3yxyl';
+    
+        emailjs.send(serviceID, templateID, params)
+        .then(
+            res =>{
+                document.getElementById('form-nombre').value = '';
+                document.getElementById('form-mail').value = '';
+                document.getElementById('form-msg').value = '';
+                console.log(res);
+        })
+        .catch((err) => console.log(err));
+    };
+    sendMail();
     exito.classList.remove('d-none');
 }
 
